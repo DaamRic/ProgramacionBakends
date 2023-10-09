13ff95c944b6b82e9a08c3dc61c7f04d4001f3c1
Damian Ricardi <damianricardi96@gmail.com>
2023-10-08 21:58:56 -0300

    Actualizado

--- ProductsManager.js
+++ ProductsManager.js
@@ -1,75 +1,137 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
+constructor() {
     this.products = [];
-    }
+    this.filePath = 'products.json';
+    this.loadProductsFromFile();
+}
 
-    generateUniqueId() {
-        return '' + Math.random().toString(36).substr(2, 9);
-    }
+generateUniqueId() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
 
-    getProducts() {
-    return this.products;
-    }
-
-    addProduct(productData) {
-    const { title, description, price, thumbnail, code, stock } = productData;
-    
-    if (this.products.some(product => product.code === code)) {
-        throw new Error('El código ya está en uso');
-    }
-
-    const id = this.generateUniqueId();
-    const product = {
-        id,
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-    };
-
-    this.products.push(product);
-    return product;
-    }
-
-    getProductById(id) {
-    const product = this.products.find(product => product.id === id);
-    if (!product) {
-        throw new Error('Producto no encontrado');
-    }
-    return product;
+loadProductsFromFile() {
+    try {
+    const data = fs.readFileSync(this.filePath, 'utf8');
+    this.products = JSON.parse(data);
+    } catch (error) {
+    this.products = [];
     }
 }
 
+saveProductsToFile() {
+    const data = JSON.stringify(this.products, null, 2);
+    fs.writeFileSync(this.filePath, data, 'utf8');
+}
+
+getProducts() {
+    return this.products;
+}
+
+addProduct(productData) {
+    const codeExists = this.products.some((product) => product.code === productData.code);
+
+    if (codeExists) {
+    throw new Error('El código del producto ya está en uso.');
+    }
+
+    const productId = this.generateUniqueId();
+
+    const newProduct = {
+    id: productId,
+    ...productData,
+    };
+
+    this.products.push(newProduct);
+
+    this.saveProductsToFile();
+
+    return productId;
+}
+
+getProductById(productId) {
+    const product = this.products.find((product) => product.id === productId);
+
+    if (!product) {
+    throw new Error('Producto no encontrado.');
+    }
+
+    return product;
+}
+
+updateProduct(productId, updatedData) {
+    const productIndex = this.products.findIndex((product) => product.id === productId);
+
+    if (productIndex === -1) {
+    throw new Error('Producto no encontrado.');
+    }
+
+    updatedData.id = productId;
+
+    this.products[productIndex] = updatedData;
+
+    this.saveProductsToFile();
+    console.log(products)
+}
+
+deleteProduct(productId) {
+    const productIndex = this.products.findIndex((product) => product.id === productId);
+
+    if (productIndex === -1) {
+    throw new Error('Producto no encontrado.');
+    }
+
+    this.products.splice(productIndex, 1);
+
+    this.saveProductsToFile();
+}
+}
 
 const productManager = new ProductManager();
 
-
-const product1 = productManager.addProduct({
-    title: 'Producto prueba',
-    description: 'Este producto es una prueba',
-    price: 10,
-    thumbnail: 'No hay imagen',
-    code: 'Code123',
-    stock: 10,
-});
-
-console.log(productManager.getProducts()); 
+const products = productManager.getProducts();
+console.log(products); 
 
 try {
-    productManager.addProduct({
-    title: 'producto repetible',
-    description: 'Este es un producto repetible',
-    price: 100,
-    thumbnail: 'imagen',
-    code: 'Code123',
-    stock: 100,
-    });
+const productId = productManager.addProduct({
+    title: 'producto prueba',
+    description: 'Este es un producto prueba',
+    price: 200,
+    thumbnail: 'Sin imagen',
+    code: 'abc123',
+    stock: 25,
+});
+
+console.log('Producto agregado con ID:', productId);
 } catch (error) {
-    console.error(error.message);
+console.error(error.message);
 }
 
+const updatedProducts = productManager.getProducts();
+console.log(updatedProducts);
 
-const productById = productManager.getProductById(product1.id);
-console.log(productById);
+try {
+const productById = productManager.getProductById(products[0].id);
+console.log('Producto encontrado por ID:', productById);
+} catch (error) {
+console.error(error.message);
+}
+
+try {
+productManager.updateProduct(products[0].id, {
+    title: 'Producto actualizado',
+    price: 300,
+});
+
+console.log('Producto actualizado con éxito.');
+} catch (error) {
+console.error(error.message);
+}
+
+try {
+productManager.deleteProduct(products[0].id);
+console.log('Producto eliminado con éxito.');
+} catch (error) {
+console.error(error.message);
+}
